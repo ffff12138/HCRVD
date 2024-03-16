a258969e767f03f31e353bc5badb386034024c02
ssong12138@163.com <ssong12138@163.com>
2024-03-17 03:14:28 +0800

    Signed-off-by: ssong12138@163.com <ssong12138@163.com>

---
+++ build_parser/build_parser.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+from tree_sitter import Language, Parser
+
+Language.build_library(
+  'c.so',
+  [
+    '../vendor/tree-sitter-c',
+  ]
+=======
+from tree_sitter import Language, Parser
+
+Language.build_library(
+  'c.so',
+  [
+    '../vendor/tree-sitter-c',
+  ]
+>>>>>>> 00b7fc3 (Signend-off-by: Song <ssong12138@163.com>)
+)
